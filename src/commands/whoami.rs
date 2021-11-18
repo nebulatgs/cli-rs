e7db0a05e29843d04556812a12078b69f2dff82a
nebulatgs <infinitewormhole@gmail.com>
2021-11-18 00:45:44 +0000

    feat: implement whoami command

--- src/commands/whoami.rs
+++ src/commands/whoami.rs
@@ -1,8 +1,29 @@
 use clap::Parser;
+use colored::Colorize;
+use graphql_client::reqwest::post_graphql;
+
+use crate::{
+	gql::queries::get_user,
+	gql::queries::GetUser,
+	util::{client::GQLClient, config::Configs},
+};
 
 #[derive(Parser)]
 pub struct Args;
 
-pub async fn command(args: Args) -> super::CommandResult {
-	todo!("Whoami command used!");
+pub async fn command(_args: Args) -> super::CommandResult {
+	let config = Configs::new().await?;
+	let client = GQLClient::new_authorized(&config)?;
+	let res = post_graphql::<GetUser, _>(
+		&client,
+		format!("{}/graphql", Configs::get_host()),
+		get_user::Variables {},
+	)
+	.await?;
+	let body: get_user::ResponseData = res.data.ok_or("Failed to retrieve response body")?;
+
+	let name = body.me.name.ok_or("Failed to retrieve name")?;
+	let email = body.me.email;
+	println!("👋 Hey {} ({})", name, email.purple());
+	Ok(())
 }
