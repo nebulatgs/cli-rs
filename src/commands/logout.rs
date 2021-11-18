48c30fd1223a5fd02887099c606f9d6355f618f9
nebulatgs <infinitewormhole@gmail.com>
2021-11-18 00:53:18 +0000

    feat: implement logout command

--- src/commands/logout.rs
+++ src/commands/logout.rs
@@ -1,8 +1,36 @@
 use clap::Parser;
+use colored::Colorize;
+use graphql_client::reqwest::post_graphql;
+
+use crate::{
+	gql::mutations::logout,
+	gql::mutations::Logout,
+	util::{client::GQLClient, config::Configs},
+};
 
 #[derive(Parser)]
 pub struct Args;
 
 pub async fn command(args: Args) -> super::CommandResult {
-	todo!("Logout command used!");
+	let config = Configs::new().await?;
+	if config.root_config.user.token.is_none() {
+		eprintln!("{}", "🚪  Already logged out".yellow());
+		return Ok(());
+	}
+	if let Some(token) = &config.root_config.user.token {
+		if token.is_empty() {
+			eprintln!("{}", "🚪  Already logged out".yellow());
+			return Ok(());
+		}
+	}
+	let client = GQLClient::new_authorized(&config)?;
+	let res = post_graphql::<Logout, _>(
+		&client,
+		format!("{}/graphql", Configs::get_host()),
+		logout::Variables {},
+	)
+	.await?;
+	res.data.ok_or("Failed to retrieve response body")?;
+	println!("{}", "👋 Logged out".yellow());
+	Ok(())
 }
